a321c6546ce8b5b8f23e466462198a98ccc8e5a3
AcruxTech <acrux_tech@mail.ru>
2022-08-18 17:44:36 +0300

    сравнение скорости инвертирования бит

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace Sandbox
 {
@@ -6,7 +7,20 @@ namespace Sandbox
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            int x = 123456789;
+            int y = ~x;
+            y += 1;
+            stopwatch.Stop();
+            Console.WriteLine($"Инвертирование бит\nTicks: {stopwatch.ElapsedTicks}\nValue: {y}\n-------------");
+
+            Stopwatch stopwatch2 = new Stopwatch();
+            stopwatch2.Start();
+            int x2 = 123456789;
+            int y2 = x2 * -1;
+            stopwatch2.Stop();
+            Console.WriteLine($"Умножение\nTicks: {stopwatch2.ElapsedTicks}\nValue: {y2}\n-------------");
         }
     }
 }
